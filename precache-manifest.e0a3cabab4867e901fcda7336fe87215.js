a9cf7038dd0a19f24f3972aaee3b03665f869b73
endayusma <enda.nur.a.y@mail.ugm.ac.id>
2020-04-15 15:04:06 +0700

    Updates

---
+++ precache-manifest.e0a3cabab4867e901fcda7336fe87215.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9616e171c95f45cb4226a04cebfde717",
+    "url": "/WEB3-15103-UTS/index.html"
+  },
+  {
+    "revision": "049b3d5602348f8ec890",
+    "url": "/WEB3-15103-UTS/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "ef97c4ed79ac04254cfe",
+    "url": "/WEB3-15103-UTS/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "049b3d5602348f8ec890",
+    "url": "/WEB3-15103-UTS/static/js/2.0f9d7ce5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/WEB3-15103-UTS/static/js/2.0f9d7ce5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef97c4ed79ac04254cfe",
+    "url": "/WEB3-15103-UTS/static/js/main.1b1ba880.chunk.js"
+  },
+  {
+    "revision": "8b9b00da64ae61d4ab10",
+    "url": "/WEB3-15103-UTS/static/js/runtime-main.63f6f857.js"
+  }
+]);
